a152301a859428f53fd69f976827eebe7cef3877
saeedkhan-github <saeedullah321@gmail.com>
2023-06-17 02:55:47 +0500

    last commit reverted

--- cypress/e2e/notes.cy.js
+++ cypress/e2e/notes.cy.js
@@ -6,4 +6,4 @@ Whats up??
 new lin eelle
 Learning Cypress from Saeed bhai the great 
 Enjoying learning CYPRESS AUTOMATION TILL NOW
-Yahoooo I learnt a few commands today. Major thanks to Saeed bhai the great :saluting_face:
+Yahoooo I learnt a few commands today. Major thanks to Saeed bhai the 
