4273dce6fbe7e23059602776f38d551f8644cd20
saeedkhan-github <saeedullah321@gmail.com>
2023-06-26 23:31:27 +0500

    uses login function in testcase

--- cypress/e2e/Assessment/Send-assessment.cy.js
+++ cypress/e2e/Assessment/Send-assessment.cy.js
@@ -1,31 +1,32 @@
+import { Login } from "../Login.cy";
+beforeEach(function() {
+
+    Login();
+    cy.wait(3000);
+    
+  });
+
 describe('My First Test', () => {
     it('click on alert',()=>{
-        cy.visit('https://webapp.hucu.us/login');
-        cy.get('.fluid > input').type('joy');
-        cy.get('.large').click();
-        cy.get('form').submit();
-        cy.get('.icon > input').click();
-        cy.get('.icon > input').type('Zoha@123');
-        cy.get('.large').click();
-        cy.get('form').submit();
-        cy.get('.pincode-input-text:nth-child(1)').type('1');
-        cy.get('.pincode-input-text:nth-child(2)').type('1');
-        cy.get('.pincode-input-text:nth-child(3)').type('1');
-        cy.get('.pincode-input-text:nth-child(4)').type('1');
-        cy.get('.pincode-input-text:nth-child(1)').type('1');
-        cy.get('.pincode-input-text:nth-child(2)').type('1');
-        cy.get('.pincode-input-text:nth-child(3)').type('1');
-        cy.get('.pincode-input-text:nth-child(4)').type('1');
-        cy.wait(10000)
+        
+        cy.wait(3000)
 
         //Send Assessment
-        cy.get(':nth-child(5) > :nth-child(1) > .content').click()
-        cy.wait(4000)
+        cy.get(':nth-child(1) > .content > .patient-info').click();
+        //cy.get(':nth-child(5) > :nth-child(1) > .content').click()
+        cy.wait(2000)
         cy.get('.icon_form > .ui').click()
         cy.get('.active > .menu > :nth-child(2)').click()
         cy.wait(2000)
         cy.get('.message-actions > .tiny').click()
 
+        // send Assessment form in the family channel 
+
+        cy.get('.family-icon').click();
+        cy.get('.icon_form > .ui').click()
+        cy.get('.active > .menu > :nth-child(2)').click()
+        cy.wait(2000)
+        cy.get('.message-actions > .tiny').click()
 
     })
 
